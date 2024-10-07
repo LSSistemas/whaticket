852ecbd8af4f15ed3373e0a0e39bc43912e7af97
Luiz Sergio <lsergiozq@gmail.com>
2024-10-07 15:19:14 -0300

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -6,4 +6,7 @@ app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(process.env.PORT);
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
