c06ec4f739d129089b7aa20250ad2913423cc3ad
Luiz Sergio <lsergiozq@gmail.com>
2024-10-07 15:25:09 -0300

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -1,11 +1,17 @@
-//simple express server to run frontend production build;
+// simple express server to run frontend production build;
 const express = require("express");
 const path = require("path");
+const dotenv = require("dotenv");
+
+//npm install dotenv - para instalar
+dotenv.config();  // Carrega as variáveis de ambiente do arquivo .env
+
 const app = express();
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
-	res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
 const port = Number(process.env.PORT) || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
